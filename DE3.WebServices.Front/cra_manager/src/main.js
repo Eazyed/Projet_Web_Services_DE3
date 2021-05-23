67d06e03851486aed0c647fbb42d2f5ebf1baba7
Quentin Thizy <thizy.quentin@gmail.com>
2021-05-23 23:58:38 +0200

    Ajout d'un tableau avec Input + somme des colonnes

--- DE3.WebServices.Front/cra_manager/src/main.js
+++ DE3.WebServices.Front/cra_manager/src/main.js
@@ -2,6 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +21,40 @@ new Vue({
     return h(App);
   },
 }).$mount("#app");
+
+// new Vue({
+//   el: '#home',
+//   data: {
+//     appName: 'My App',
+//     items: [
+//       { name: 'Apple', qty: 5 },
+//       { name: 'Banana', qty: 10 },
+//     ],
+//     fields: [
+//       {
+//         key: 'index',
+//         label: '#',
+//       },
+//       {
+//         key: 'name',
+//         label: 'fruit',
+//       },
+//       {
+//         key: 'qty',
+//         label: 'quantity',
+//         thClass: 'red',
+//       },
+//       {
+//         key: 'action',
+//       },
+//     ],
+//   },
+//   methods: {
+//     saveFruit (fruit) {
+//       console.log(`Saving fruit: ${fruit}`);
+//     },
+//   },
+//   mounted () {
+    
+//   }
+// });
--- DE3.WebServices.Front/cra_manager/src/views/Home.vue
+++ DE3.WebServices.Front/cra_manager/src/views/Home.vue
@@ -1,18 +1,173 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+      <b-container class="mt-2">
+        <!-- <b-table striped hover :items="items" :fields="fields"> -->
+        <b-table striped hover :items="items">
+          <template v-slot:cell(projet)="row">
+            <b-form-input v-model="row.item.projet"/>
+          </template>
+          <template v-slot:cell(lundi)="row">
+            <b-form-input v-model="row.item.lundi"/>
+          </template>
+          <template v-slot:cell(mardi)="row">
+            <b-form-input v-model="row.item.mardi"/>
+          </template>
+          <template v-slot:cell(mercredi)="row">
+            <b-form-input v-model="row.item.mercredi"/>
+          </template>
+          <template v-slot:cell(jeudi)="row">
+            <b-form-input v-model="row.item.jeudi"/>
+          </template>
+          <template v-slot:cell(vendredi)="row">
+            <b-form-input v-model="row.item.vendredi"/>
+          </template>
+          <template v-slot:cell(samedi)="row">
+            <b-form-input v-model="row.item.samedi"/>
+          </template>
+          <template v-slot:cell(dimanche)="row">
+            <b-form-input v-model="row.item.dimanche"/>
+          </template>
+          <template slot="footer">
+            <td>Total</td>
+            <td>{{ totalLundi }}</td>
+            <td>{{ totalMardi }}</td>
+            <td>{{ totalMercredi }}</td>
+            <td>{{ totalJeudi }}</td>
+            <td>{{ totalVendredi }}</td>
+            <td>{{ totalSamedi }}</td>
+            <td>{{ totalDimanche }}</td>
+          </template>
+        </b-table>
+        <table>
+          <tr>
+            <td>Total</td>
+            <td>{{ totalLundi }}</td>
+            <td>{{ totalMardi }}</td>
+            <td>{{ totalMercredi }}</td>
+            <td>{{ totalJeudi }}</td>
+            <td>{{ totalVendredi }}</td>
+            <td>{{ totalSamedi }}</td>
+            <td>{{ totalDimanche }}</td>
+          </tr>
+        </table>
+        {{ items }}
+      </b-container>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style scoped>
+table th, table td {
+    padding: 0.75rem;
+    vertical-align: top;
+    border-top: 1px solid #dee2e6;
+}
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+td {
+    display: table-cell;
+    vertical-align: inherit;
+}
+
+table {
+    width: 100%;
+    margin-bottom: 1rem;
+    color: #212529;
+    table-layout: fixed;
+    border-collapse: collapse;
+    text-indent: initial;
+    border-spacing: 2px;
+}
+</style>
+
+<script>
+  export default {
+  data() {
+    return {
+      fields: { Projet: "projet", Lundi: "lundi", Mardi: "mardi", Mercredi: "mercredi", Jeudi: "jeudi", Vendredi: "vendredi", Samedi: "samedi", Dimanche: "dimanche" },
+      items: [
+        {
+          projet: "Projet 1",
+          lundi: 1,
+          mardi: 2,
+          mercredi: 3,
+          jeudi: 4,
+          vendredi: 5,
+          samedi: 6,
+          dimanche: 7
+        },
+        {
+          projet: "Projet 2",
+          lundi: 0,
+          mardi: 0,
+          mercredi: 0,
+          jeudi: 0,
+          vendredi: 0,
+          samedi: 0,
+          dimanche: 0
+        },
+        {
+          projet: "Projet 3",
+          lundi: 0,
+          mardi: 0,
+          mercredi: 0,
+          jeudi: 0,
+          vendredi: 0,
+          samedi: 0,
+          dimanche: 0
+        }
+      ]
+    }
   },
-};
+  computed: {
+    totalLundi () {
+      console.log('get lundi total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.lundi), 0);
+    },
+    
+    totalMardi () {
+      console.log('get mardi total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.mardi), 0);
+    },
+    
+    totalMercredi () {
+      console.log('get mercredi total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.mercredi), 0);
+    },
+
+    totalJeudi () {
+      console.log('get jeudi total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.jeudi), 0);
+    },
+
+    totalVendredi () {
+      console.log('get vendredi total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.vendredi), 0);
+    },
+
+    totalSamedi () {
+      console.log('get samedi total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.samedi), 0);
+    },
+
+    totalDimanche () {
+      console.log('get dimanche total')
+      return this.items.reduce((acc, cur) => acc + Number(cur.dimanche), 0);
+    }
+  }
+  // methods: {
+  //   getTotal () {
+  //     console.log('method getting current total')
+  //     return this.regions.reduce((acc, cur) => acc + Number(cur.lundi), 0);
+  //   },
+  //   updateDatabase(){
+  //     console.log("update table with total: "+ this.totalCurrent());
+  //   }
+  // }
+      // };
+    // }
+  };
+
+  // let test =  data.items;
+
+  console.log("test");
 </script>
+
